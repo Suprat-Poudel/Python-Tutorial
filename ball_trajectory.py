614951addde106537e753dcb10d310f2cea74c74
Suprat <poudel.suprat@gmail.com>
2022-04-05 12:03:38 +0530

    added ball trajectory

---
+++ ball_trajectory.py
@@ -0,0 +1,8 @@
+import math
+x =int(input("Enter horizontal component (x): "))
+g =float(input("Enter acceleration due to gravity (g): "))
+v=float(input("Enter initial velocity (v): "))
+theta=float(input("Enter angle: "))
+y= float(input("position of the ball (y): "))
+ans= x*math.tan(theta)-((g*x*x)/(2*v*v*math.cos(theta)*math.cos(theta)))+ y
+print(ans)
